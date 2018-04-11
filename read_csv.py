17c426f29e6422c171e9c4a489d5a1b801351bd6
gauravssnl <gaurav.ssnl@gmail.com>
2018-04-11 21:28:13 +0530

    Added read_csv.py

---
+++ read_csv.py
@@ -0,0 +1,33 @@
+import csv
+import datetime
+
+
+def read_csv():
+	today = str(datetime.date.today())
+	year, month, day = today.split("-")
+	date = "{}-{}-{}".format(day, month, year)
+	print("Today's Date: ", date)
+	filename = "EQ{}{}{}.CSV".format(day, month, year[2:])
+	print(filename)
+	# File context
+	with open(filename, "r") as f:
+		reader = csv.reader(f)
+		row_num = 0
+		for row in reader:
+			if row_num == 0:
+				header = row
+				for h in header:
+					print(h, end="  |  ")
+				print()
+				row_num += 1
+			else:
+				colnum = 0
+				for col in row:
+					print(col, end=" "*len(header[colnum]))
+					colnum += 1
+				print()
+
+
+
+if __name__ == '__main__':
+		read_csv()	
